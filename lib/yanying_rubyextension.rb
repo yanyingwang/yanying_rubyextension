a9b957fcf11c0f34337fe7b28c5888a0582c5580
YanyingWang <yanyingwang1@gmail.com>
2017-05-11 12:25:08 +0800

    active admin base

--- lib/yanying_rubyextension.rb
+++ lib/yanying_rubyextension.rb
@@ -4,6 +4,22 @@ module YanyingRubyextension
 
 
 
+  models = ActiveRecord::Base.connection.tables.map do |model|
+    model.capitalize.singularize.camelize
+  end
+
+  arrs = [ 'Admin', 'SchemaMigration', 'SmsLog', 'Base', '..', '.' ]
+  arrs += Rails.root.join('app/admin').entries.map{ |f| f.sub_ext('').to_s.camelize }
+  arrs.each { |e| models.delete(e) }
+
+  models.each do |m|
+    ActiveAdmin.register m.constantize do
+      menu parent: "其他"
+      permit_params model.column_names
+      actions :all, except: [ :destroy ]
+    end
+  end
+
 
   class Array
     def match?(value)
